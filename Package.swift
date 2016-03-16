1ec8348be79c27abd8bbe9bed7eac4c49b842b77
antonmes <antonmes@users.noreply.github.com>
2016-03-16 07:26:54 +0300

    Swift 3 renaming

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MediaTypeSerializerCollection",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 4)
     ]
 )
